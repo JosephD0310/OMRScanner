6222dd0c7887a08024e7d361353c9169e51dc9ac
JosephD0310 <dangnguyenhoang8@gmail.com>
2025-03-20 16:10:34 +0700

    update requirements

--- api/main.py
+++ api/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=["https://omr-scanner-puce.vercel.app/"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
